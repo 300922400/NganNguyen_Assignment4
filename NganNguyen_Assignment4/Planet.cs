107c603ac85ca796ab12d4bb63a7614b6da1ba5b
300922400 <300922400@PR-A313-26>
2017-07-26 13:22:52 -0400

    Add project files.

---
+++ NganNguyen_Assignment4/Planet.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Name: Ngan Nguyen
+ * Date: July 25, 2017
+ * Description: This is the abstract class Planet which other subclasses will
+ * derive from.
+ * Version: 0.1 - Create The Abstract Class Planet
+ */
+
+namespace NganNguyen_Assignment4
+{
+    /// <summary>
+    /// This is the abstract class planet
+    /// </summary>
+     abstract class Planet
+    {
+        // PRIVATE FIELDS (INSTANCE VARIABLES)
+         private double _diameter;
+         private double _mass;
+         private int _moonCount;
+         private string _name;
+         private double _orbitalPeriod;
+         private int _ringCount;
+         private double _rotationPeriod;
+         
+        // PUBLIC PROPERTIES -----------------------------------------
+         public readonly double Diameter
+         {
+             get
+             {
+                 return this._diameter;
+             }
+         }
+         public readonly double Mass
+         {
+             get
+             {
+                 return this._mass;
+             }
+         }
+         public int MoonCount
+         {
+             get
+             {
+                 return this._moonCount;
+             }
+             set 
+             {
+                 this._moonCount = value;
+             }
+         }
+         public readonly string Name
+         {
+             get
+             {
+                 return this._name;
+             }
+         }
+         public double OrbitalPeriod
+         {
+             get
+             {
+                 return this._orbitalPeriod;
+             }
+             set
+             {
+                 this._orbitalPeriod = value;
+             }
+         }
+         public int RingCount
+         {
+             get
+             {
+                 return this._ringCount;
+             }
+             set
+             {
+                 this._ringCount = value;
+             }
+         }
+         public double RotationPeriod
+         {
+             get
+             {
+                 return this._rotationPeriod;
+             }
+             set
+             {
+                 this._rotationPeriod = value;
+             }
+         }
+
+        // CONSTRUCTORS -----------------------------------------------
+         /// <summary>
+         /// This is the constructor for the abstract class Planet
+         /// it requires three parameter - name(string),diameter(double),mass(double)
+         /// </summary>
+         /// <param name="name"></param>
+         /// <param name="diameter"></param>
+         /// <param name="mass"></param>
+         public Planet(string name,double diameter,double mass)
+         {
+             this._name = name;
+             this._diameter = diameter;
+             this._mass = mass;
+         }
+        // PUBLIC ABSTRACT METHODS
+         public override string ToString()
+         {
+             return string.Format("Name:{0},Diameter:{1},Mass:{2}",Name,Diameter,Mass);
+         }
+    }
+}
