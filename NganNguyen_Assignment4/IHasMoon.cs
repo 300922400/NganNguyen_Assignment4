653008690bf9088f7e56095d1ec4f4902e586e0d
ngan <ngan@NGAN-PC>
2017-07-28 12:12:59 -0400

    Implement the GiantPlanet Class and TerrestrialPlanet Class in driver class

--- NganNguyen_Assignment4/IHasMoon.cs
+++ NganNguyen_Assignment4/IHasMoon.cs
@@ -6,7 +6,8 @@ using System.Text;
 /*
  * Name: Ngan Nguyen - ID: 300922400
  * Date: July 27, 2017
- * Description: This is IHasMoon Interface
+ * Description: This is IHasMoon Interface define the HasMoon method that must be 
+ * implemented in any class subscribes it 
  * Version: 0.1 - create the IHasMoon Interface
  */
 namespace NganNguyen_Assignment4
--- NganNguyen_Assignment4/Planet.cs
+++ NganNguyen_Assignment4/Planet.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 /*
  * Name: Ngan Nguyen - ID:300922400
  * Date: July 25, 2017
