a06d493a245582be399dd7f76858d68a02a132c3
ngan <ngan@NGAN-PC>
2017-07-28 12:18:11 -0400

    Modify the first tria

--- NganNguyen_Assignment4/Program.cs
+++ NganNguyen_Assignment4/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name: Ngan Nguyen - ID:300922400
  * Date: July 28, 2017
  * Description: implement the GiantPlanet class and TerrestrialPlanet class
- * Version: 0.1 - First trial to check the output
+ * Version: 0.2- Modify the first trial to check the output 
  */
 
 namespace NganNguyen_Assignment4
@@ -17,7 +17,7 @@ namespace NganNguyen_Assignment4
     {
         static void Main(string[] args)
         {
-            GiantPlanet Jupiter = new GiantPlanet("Jupiter", 69911,1.88*Math.Pow(10.00,27.00), "Gas");
+            GiantPlanet Jupiter = new GiantPlanet("Jupiter", 69911,1.88*Math.Exp(27.00), "Gas");
             Jupiter.MoonCount = 4;
             Jupiter.RingCount = -2;
             Console.WriteLine(Jupiter.ToString());
@@ -25,9 +25,10 @@ namespace NganNguyen_Assignment4
             //Console.WriteLine("The types is" + Mars.GetType());
             Console.WriteLine("Has Moons on this planet?" + Jupiter.HasMoons());
             Console.WriteLine("Has Rings on this planet?" + Jupiter.HasRings());
-            TerrestrialPlanet Mars = new TerrestrialPlanet("Mars", 4200, 6.39 * Math.Pow(10.00, 23.00), false);
+            TerrestrialPlanet Mars = new TerrestrialPlanet("Mars", 4200, 6.39 * Math.Exp(23.00), false);
             Mars.MoonCount = 0;
             Jupiter.RingCount = -1;
+            Console.WriteLine(Mars.ToString());
             Console.WriteLine("Has Moons on this planet?" + Mars.HasMoons());
             Console.WriteLine("Has Habitable on this planet?" + Mars.Habitable());
             //display oxygen??
